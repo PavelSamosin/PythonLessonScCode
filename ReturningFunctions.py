45c4ca6fbf27548f1eeef5ba18bfdf1f8788e778
PavelSamosin <48923458+PavelSamosin@users.noreply.github.com>
2019-03-26 13:14:02 -0400

    Add files via upload

---
+++ ReturningFunctions.py
@@ -0,0 +1,8 @@
+
+
+def return_name(firstName, lastName):
+    return print("FirstName: " + firstName + "\nLastName: " + lastName)
+
+first = input("What is your first name?")
+last = input ("What is your last name?")
+return_name(first, last)
---
+++ functions.py
@@ -0,0 +1,11 @@
+#-------------------------Using functions with parameters------------------------------
+
+#Created a function that calculates, and has 3 parameters, 2 for the numbers
+def add_Two_Numbers (num1, num2):
+    answer = num1 + num2
+    print(answer)
+
+
+#Now we call the function
+
+add_Two_Numbers(5,7)
